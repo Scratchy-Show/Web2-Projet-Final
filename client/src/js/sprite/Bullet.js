b224871a9c1975a2e8025e28a7a7d1e4d01f9b7f
Scratchy-Show <couretalexandredev@gmail.com>
2023-11-24 15:07:47 -0500

    Détection de collision entre un Bullet et le robot.

--- client/src/js/sprite/Bullet.js
+++ client/src/js/sprite/Bullet.js
@@ -1,4 +1,5 @@
 import { TiledImage } from '../TiledImage.js';
+import { ChaingunRobotWaiting } from './ChaingunRobotWaiting.js';
 import { spriteListRegister } from '../page-register.js';
 
 export class Bullet{
@@ -29,22 +30,43 @@ export class Bullet{
         this.TiledImageBullet.changeMinMaxInterval(0, 0);
     }
 
+    removeBullet() {
+        const index = spriteListRegister.indexOf(this);
+        if (index !== -1) {
+            spriteListRegister.splice(index, 1);
+        }
+
+        if (this.nodeBullet.parentNode) {
+            this.nodeBullet.parentNode.removeChild(this.nodeBullet);
+        }
+    }
+
+    checkCollisionWithRobot() {
+        let robot = spriteListRegister.find(sprite => sprite instanceof ChaingunRobotWaiting);
+
+        if (robot && this.bulletX >= robot.robotX) {
+            return true;
+        }
+        return false;
+    }
+
     tick () {
         // Déplacement
         this.bulletX += this.speed;
 
         // Si la balle est hors de l'écran
         if (this.bulletX > window.innerWidth) {
-            // Supprime Bullet
-            const index = spriteListRegister.indexOf(this);
-            if (index !== -1) {
-                spriteListRegister.splice(index, 1);
-            }
-            
-            this.nodeBullet.remove();
-        } else
-            this.TiledImageBullet.tick(this.bulletX, this.bulletY);
+            this.removeBullet();
+        } else {
+            // Vérifie la collision avec le robot
+            let hitRobot = this.checkCollisionWithRobot();
 
+            if (hitRobot) {
+                this.removeBullet();
+            } else {
+                this.TiledImageBullet.tick(this.bulletX, this.bulletY);
+            }
+        }
         return true;
     }
 }
