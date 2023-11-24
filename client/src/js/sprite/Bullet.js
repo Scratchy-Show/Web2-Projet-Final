c13909fe01add4daf236f871f2365dbfdb112a97
Scratchy-Show <couretalexandredev@gmail.com>
2023-11-24 15:42:47 -0500

    Gestion de l'opacité du robot lorsqu'il est touché par une balle.

--- client/src/js/sprite/Bullet.js
+++ client/src/js/sprite/Bullet.js
@@ -63,6 +63,7 @@ export class Bullet{
 
             if (hitRobot) {
                 this.removeBullet();
+                ChaingunRobotWaiting.reduceOpacity();
             } else {
                 this.TiledImageBullet.tick(this.bulletX, this.bulletY);
             }
--- client/src/js/sprite/ChaingunRobotWaiting.js
+++ client/src/js/sprite/ChaingunRobotWaiting.js
@@ -1,14 +1,17 @@
 import { TiledImage } from '../TiledImage.js';
 
 export class ChaingunRobotWaiting{
+    static opacity = 1;
+
     constructor(x, y) {
         let colCount = 16;
         let rowCount = 1;
-        let refreshDelay = 100;
+        let refreshDelay = 100;//
         let loopColumns = true;
         let scale = 1.7;
         this.robotX = x + x;
         this.robotY = y;
+        this.opacity = 1;
 
         /***** SPRITESHEET - CHAINGUN ROBOT WAITING  *****/
         this.nodeChaingunRobotWaiting = document.createElement("div");
@@ -25,6 +28,21 @@ export class ChaingunRobotWaiting{
             this.nodeChaingunRobotWaiting
         );
         this.TiledImageChaingunRobotWaiting.changeMinMaxInterval(0, 15);
+    }
+
+    static reduceOpacity() {
+        if (ChaingunRobotWaiting.opacity > 0) {
+            ChaingunRobotWaiting.opacity -= 0.2;
+            ChaingunRobotWaiting.setOpacity(ChaingunRobotWaiting.opacity);
+        }
+    }
+
+    static setOpacity(opacity) {
+        if (opacity > 1) opacity = 1;
+        if (opacity < 0) opacity = 0;
+        ChaingunRobotWaiting.opacity = opacity;
+
+        document.querySelector(".chaingun-robot-waiting").style.opacity = ChaingunRobotWaiting.opacity;
     }
 
     tick () {
