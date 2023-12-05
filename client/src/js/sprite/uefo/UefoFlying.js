d3258986f5adef35d7c091bd7d74db62f0b5088e
Scratchy-Show <couretalexandredev@gmail.com>
2023-12-04 20:27:28 -0500

    Déplacement de manière aléatoire des UEFO dans la fenêtre.

--- client/src/js/sprite/uefo/UefoFlying.js
+++ client/src/js/sprite/uefo/UefoFlying.js
@@ -6,12 +6,20 @@ export class UefoFlying{
         let rowCount = 1;
         let refreshDelay = 100;
         let loopColumns = true;
-        let scale = 1;
+        let scale = 1.4;
         this.memberName = memberName;
+        this.speed = 2;
+        this.speedX = (Math.random() > 0.5 ? 1 : -1) * this.speed; // +x ou -x
+        this.speedY = (Math.random() > 0.5 ? 1 : -1) * this.speed; // +y ou -y
+        this.x = Math.random() * 700 + 100;
+        this.y = Math.random() * 500 + 100;
 
         /***** SPRITESHEET - UEFO FLYING  *****/
         this.nodeUefoFlying = document.createElement("div");
         this.nodeUefoFlying.classList.add("uefo-flying-" + memberName);
+        this.nodeUefoFlying.style.zIndex = -10;
+        this.nodeUefoFlying.style.top = this.y + "px";
+        this.nodeUefoFlying.style.left = this.x + "px";
         document.querySelector(".chat-main").append(this.nodeUefoFlying);
 
         this.TiledImageUefoFlying = new TiledImage(
@@ -27,7 +35,28 @@ export class UefoFlying{
     }
 
     tick () {
-        this.TiledImageUefoFlying.tick(this.robotX, this.robotY);
+        // Calcul des nouvelles positions
+        let newX = this.x + this.speedX;
+        let newY = this.y + this.speedY;
+
+        // Vérifie si les nouvelles positions sont toujours dans les limites de la fenêtre
+        if (newX >= 0 && newX + this.nodeUefoFlying.offsetWidth <= window.innerWidth)
+            this.x = newX;
+        else
+            // Inverse la direction si la nouvelle position dépasse les limites horizontales
+            this.speedX = -this.speedX;
+
+        if (newY >= 0 && newY + this.nodeUefoFlying.offsetHeight <= window.innerHeight)
+            this.y = newY;
+        else
+            // Inverse la direction si la nouvelle position dépasse les limites verticales
+            this.speedY = -this.speedY;
+
+        // Met à jour la position
+        this.nodeUefoFlying.style.top = this.y + "px";
+        this.nodeUefoFlying.style.left = this.x + "px";
+
+        this.TiledImageUefoFlying.tick(newX, newY);
 
         return true;
     }
